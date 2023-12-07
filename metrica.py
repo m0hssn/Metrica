8b001b0041b43794e8451dc0a1cacdffc28e68e7
Seyed Mohsen Sadeghi <M0HSNN@hotmail.com>
2023-12-08 02:53:16 +0330

    Update metrica.py

--- metrica.py
+++ metrica.py
@@ -1,142 +1,68 @@
-from torchmetrics.classification import MulticlassF1Score
-from torchmetrics.classification import Accuracy
-from torchmetrics.classification import Recall
-from torchmetrics.classification import Precision
-from torchmetrics.classification import AUROC
-from torchmetrics.classification import MulticlassAveragePrecision
-import torch
-
-
 class Metrica:
     def __init__(self, num_classes=3, task="multiclass"):
-        """
-        Initializes the Metrica class with the specified number of classes and task type.
-        Args:
-            num_classes (int): The number of classes for the classification task.
-            task (str): The type of classification task, either "multiclass" or "binary".
-        """
         self.num_classes = num_classes
         self.task = task
-        self.y_hat = []  # List to store predicted values
-        self.y = []  # List to store ground truth values
+        self.reset_metrics()
+
+    def reset_metrics(self):
+        # Initialize necessary metrics
+        self.metric_f1 = MulticlassF1Score(num_classes=self.num_classes)
+        self.metric_acc = Accuracy(num_classes=self.num_classes, task=self.task)
+        self.metric_recall = Recall(num_classes=self.num_classes, task=self.task)
+        self.metric_precision = Precision(num_classes=self.num_classes, task=self.task)
+        self.metric_auroc = AUROC(num_classes=self.num_classes, task=self.task)
+        self.metric_auprc = MulticlassAveragePrecision(num_classes=self.num_classes, thresholds=None)
+        self.metric_mcc = MatthewsCorrcoef(num_classes=self.num_classes)
+        self.metric_fbeta = FBeta(num_classes=self.num_classes, beta=0.5)
+        
+        # Initialize variables for TP, TN, FP, FN
+        self.TP = torch.zeros(self.num_classes)
+        self.TN = torch.zeros(self.num_classes)
+        self.FP = torch.zeros(self.num_classes)
+        self.FN = torch.zeros(self.num_classes)
 
     def upgrade(self, y_hat: torch.tensor, y: torch.tensor):
-        """
-        Method to upgrade the metric by adding new predicted and ground truth values.
-        Args:
-            y_hat (torch.tensor): Predicted values
-            y (torch.tensor): Ground truth values
-        """
-        self.y.append(y)
-        self.y_hat.append(y_hat)
-
-    def reset(self):
-        """
-        Method to reset the metric by clearing the stored predicted and ground truth values.
-        """
-        self.y_hat = []
-        self.y = []
+        # Update metrics
+        self.metric_f1.update(y_hat, y)
+        self.metric_acc.update(y_hat, y)
+        self.metric_recall.update(y_hat, y)
+        self.metric_precision.update(y_hat, y)
+        self.metric_auroc.update(y_hat, y)
+        self.metric_auprc.update(y_hat, y)
+        self.metric_mcc.update(y_hat, y)
+        self.metric_fbeta.update(y_hat, y)
+        
+        # Calculate TP, TN, FP, FN
+        for i in range(self.num_classes):
+            self.TP[i] += ((y_hat == i) & (y == i)).sum()
+            self.TN[i] += ((y_hat != i) & (y != i)).sum()
+            self.FP[i] += ((y_hat == i) & (y != i)).sum()
+            self.FN[i] += ((y_hat != i) & (y == i)).sum()
 
     def f1_score(self, average='macro'):
-        """
-        Method to calculate the F1 score for the stored predicted and ground truth values.
-        Returns:
-            float: The calculated F1 score.
-        """
-        y = torch.cat(self.y).to('cpu')
-        y_hat = torch.cat(self.y_hat).to('cpu')
-
-        metric = MulticlassF1Score(num_classes=self.num_classes, average=average)
-
-        return metric(y_hat, y)
+        return self.metric_f1.compute(average=average)
 
     def accuracy(self):
-        """
-        Method to calculate the accuracy for the stored predicted and ground truth values.
-        Returns:
-            float: The calculated accuracy.
-        """
-        y = torch.cat(self.y).to('cpu')
-        y_hat = torch.cat(self.y_hat).to('cpu')
-
-        metric = Accuracy(num_classes=self.num_classes, task=self.task)
-
-        return metric(y_hat, y)
+        return self.metric_acc.compute()
 
     def recall_score(self, average='macro'):
-         """
-         Method to calculate the recall score for the stored predicted and ground truth values.
-
-         Args:
-             average (str): The type of averaging to use when calculating the recall score. Can be "macro", "micro", or "weighted".
-
-         Returns:
-             float: The calculated recall score.
-         """
-
-         y = torch.cat(self.y).to('cpu')
-         y_hat = torch.cat(self.y_hat).to('cpu')
-
-         metric = Recall(num_classes=self.num_classes, task=self.task, average=average)
-
-         return metric(y_hat, y)
+        return self.metric_recall.compute(average=average)
 
     def precision_score(self, average='macro'):
-         """
-         Method to calculate the precision score for the stored predicted and ground truth values.
+        return self.metric_precision.compute(average=average)
 
-         Args:
-             average (str): The type of averaging to use when calculating the precision score. Can be "macro", "micro", or "weighted".
-
-         Returns:
-             float: The calculated precision score.
-         """
-
-         y = torch.cat(self.y).to('cpu')
-         y_hat = torch.cat(self.y_hat).to('cpu')
-
-         metric = Precision(num_classes=self.num_classes, task=self.task, average=average)
-
-         return metric(y_hat, y)
-    
     def AUROC(self, average='macro'):
-         """
-         Method to calculate the Area Under the Receiver Operating Characteristic Curve (AUROC) for the stored predicted and ground truth values.
+        return self.metric_auroc.compute(average=average)
 
-         Args:
-             average (str): The type of averaging to use when calculating the AUROC. Can be "macro", "micro", or "weighted".
-
-         Returns:
-             float: The calculated AUROC.
-         """
-
-         y = torch.cat(self.y).to('cpu')
-         y_hat = torch.cat(self.y_hat).to('cpu')
-
-         metric = AUROC(num_classes=self.num_classes, task=self.task, average=average)
-
-         return metric(y_hat, y)
-    
     def AUPRC(self, average='macro'):
-         """
-         Method to calculate the Area Under the Precision-Recall Curve (AUPRC) for the stored predicted and ground truth values.
-
-         Args:
-             average (str): The type of averaging to use when calculating the AUPRC. Can be "macro", "micro", or "weighted".
-
-         Returns:
-             float: The calculated AUPRC.
-         """
-
-         y = torch.cat(self.y).to('cpu')
-         y_hat = torch.cat(self.y_hat).to('cpu')
-
-         metric = MulticlassAveragePrecision(num_classes=self.num_classes, average=average, thresholds=None)
-
-         return metric(y_hat, y)
+        return self.metric_auprc.compute(average=average)
+    
+    def MCC(self):
+        return self.metric_mcc.compute()
+    
+    def fbeta_score(self, average='macro'):
+        return self.metric_fbeta.compute(average=average)
     
     def print_metrics(self):
-          """
-          Method to print all metrics for stored predicted and ground truth values in one line
-          """
-          print(f'F1 Score: {self.f1_score()}, Accuracy: {self.accuracy()}, Recall Score: {self.recall_score()}, Precision Score: {self.precision_score()}, AUROC: {self.AUROC()}, AUPRC: {self.AUPRC()}')
+        print(f'F1 Score: {self.f1_score()}, Accuracy: {self.accuracy()}, Recall Score: {self.recall_score()}, Precision Score: {self.precision_score()}, AUROC: {self.AUROC()}, AUPRC: {self.AUPRC()}, MCC: {self.MCC()}, Fbeta Score: {self.fbeta_score()}')
+        print(f'TP: {self.TP}, TN: {self.TN}, FP: {self.FP}, FN: {self.FN}')
